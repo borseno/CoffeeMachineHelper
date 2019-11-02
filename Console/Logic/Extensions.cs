0330a6dd94f261d49514dd14ba75e3f326724e2b
borseno <borseno2@gmail.com>
2019-11-02 21:37:17 +0200

    added string.split method

---
+++ Console/Logic/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic
+{
+    public static class Extensions
+    {
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> enumerable, int columns)
+        {
+            var result = new List<List<T>>();
+
+            int count = 0;
+            var currentList = new List<T>(columns);
+            foreach (var i in enumerable)
+            {
+                if (count == columns)
+                {
+                    result.Add(currentList);
+                    currentList = new List<T>(columns);
+                    count = 0;
+                }
+                currentList.Add(i);
+                count++;
+            }
+
+            result.Add(currentList);
+
+            return result;
+        } 
+    }
+}
