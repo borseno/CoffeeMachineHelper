f21510ffa700ea48a890ebeb1f52665e7637215c
borseno <borseno2@gmail.com>
2019-11-02 21:37:04 +0200

    added models for problems and user state

---
+++ Console/Logic/Models/Problem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Models
+{
+    public class Problem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ShortDescription { get; set; }
+
+        public Question FirstQuestion { get; set; }
+
+        public IEnumerable<Solution> Solutions { get; set; }
+    }
+
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+       
+        public IEnumerable<Answer> Answers { get; set; }
+    }
+
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+
+        public Question Origin { get; set; }
+        public Question NextQuestion { get; set; }
+        public Solution Solution { get; set; }
+    }
+
+    public class Solution
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+
+        public Problem Problem { get; set; }
+    }
+}
--- Console/Logic/Models/UserInfo.cs
+++ Console/Logic/Models/UserInfo.cs
@@ -6,13 +6,40 @@ using Telegram.Bot.Types;
 namespace Logic.Models
 {
     public class UserInfo
-    {      
+    {   
+        public UserInfo()
+        {
+            State ??= new UserState
+            {
+                UserInfo = this
+            };
+        }
+
         public int UserId { get; set; }
         public User User { get; set; }
 
         public bool IsVerified => UtcExpireDate == null ? false : DateTime.UtcNow > UtcExpireDate.Value;
-
         public DateTime? UtcVerifyDate { get; set; }
         public DateTime? UtcExpireDate { get; set; }
+
+        public UserState State { get; set; }
+    }
+
+    public class UserState
+    {
+        public int UserId { get; set; }
+        public UserInfo UserInfo { get; set; }
+
+        public UserStage UserStage { get; set; }
+
+        public Problem Problem { get; set; }
+        public Question Question { get; set; }
+    }
+
+    public enum UserStage
+    {
+        None = 0,
+        PickingProblem = 1,
+        AnsweringQuestion = 2
     }
 }
